28355a31a9ea21eb351dc1c76d2ab5c86a63fa2d
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2020-09-16 03:19:48 +0500

    2nd

---
+++ app.js
@@ -0,0 +1,17 @@
+var countdown = new Date('November 23, 2020 00:00:00').getTime();
+
+
+var x = setInterval(function()  {
+    var current = new Date().getTime();
+    var final = countdown - current;
+
+    var days = Math.floor( final/(1000*60*60*24));
+    var hrs = Math.floor(( final % (1000*60*60*24)) / (1000*60*60));
+    var mins = Math.floor(( final % (1000*60*60)) / (1000*60));
+    var sec = Math.floor( (final % (1000*60)) / 1000);
+
+    document.getElementById('Days').textContent = days;
+    document.getElementById('Hours').textContent = hrs;
+    document.getElementById('Mins').textContent = mins;
+    document.getElementById('Sec').textContent = sec;
+},1000);
